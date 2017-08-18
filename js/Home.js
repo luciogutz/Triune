74cca66b29f9265a80f94edd3c4ecf858b3b3d33
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-08-17 19:29:11 -0500

    added another photo for background of responsive content section

--- js/Home.js
+++ js/Home.js
@@ -14,10 +14,15 @@ class Home extends React.Component {
             <div>
               <h1 className="FP-1st-title"> Responsive Design </h1>
             </div>
-            <div className="FP-1st-responsive_text-div">
-              <p>
-                In the morning people use their phones, in the afternoon their desktop, and at night their tablet. Your site needs to be ready for anything, and we can make that happen.
-              </p>
+            <div className="FP-1st-resp-content-container">
+              <div className="FP-1st-responsive_text-div">
+                <p>
+                  In the morning people use their phones, in the afternoon their desktop, and at night their tablet. Your site needs to be ready for anything, and we can make that happen.
+                </p>
+              </div>
+              <div className="FP-1st-responsiveImage">
+                <img src="https://github.com/luciogutz/Triune/blob/master/public/assets/responsiveImage.png?raw=true" />
+              </div>
             </div>
           </div>
           <div>
