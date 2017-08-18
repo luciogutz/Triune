a683063cb47c9d051c524efd390bc0f9e58fd0e1
Lucio Gutierrez <lucio.gutierrez30@yahoo.com>
2017-08-17 20:13:16 -0500

    added design icon

--- js/Home.js
+++ js/Home.js
@@ -9,18 +9,32 @@ class Home extends React.Component {
     return(
       <section>
         <Header />
-        <section className="FP-1st-section">
-          <div className="FP-1st-section-overlay">
+        <section className="three-in-one-container">
+          <div className="three-in-one-div">
+            <i className="fa fa-pencil-square-o" aria-hidden="true"></i>
+            <h3>
+              Responsive Design
+            </h3>
+          </div>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </section>
+        <section className="resp-section">
+          <div className="resp-section-overlay">
             <div>
-              <h1 className="FP-1st-title"> Responsive Design </h1>
+              <h2 className="resp-title"> Responsive Design </h2>
             </div>
-            <div className="FP-1st-resp-content-container">
-              <div className="FP-1st-responsive_text-div">
+            <div className="resp-content-container">
+              <div className="resp_text-div">
                 <p>
                   In the morning people use their phones, in the afternoon their desktop, and at night their tablet. Your site needs to be ready for anything, and we can make that happen.
                 </p>
               </div>
-              <div className="FP-1st-responsiveImage">
+              <div className="resp-Image">
                 <img src="https://github.com/luciogutz/Triune/blob/master/public/assets/responsiveImage.png?raw=true" />
               </div>
             </div>
---
+++ js/footer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { BrowserRouter, Route, Link, history } from 'react-router-dom'
+
+class Footer extends React.Component {
+  render() {
+    return(
+      <section>
+        <div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+        <div>
+      </section>
+    )
+  }
+}
+
+export default Footer
